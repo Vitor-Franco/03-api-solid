fd6dd7e0960dc10ef43d41161cdb5571e11ea028
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-05-02 05:57:07 -0300

    feature: adiciona eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0', // Facilitar o uso com o frontend
-  port: env.PORT,
-}).then(() => {
-  console.log('🔥 Server is running on port 3333');
-});
+app
+  .listen({
+    host: '0.0.0.0', // Facilitar o uso com o frontend
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🔥 Server is running on port 3333')
+  })
