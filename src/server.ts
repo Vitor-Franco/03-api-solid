4203cfd15717bd30e42fcf764e7423f98b92d857
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-05-02 05:50:46 -0300

    feature: adiciona validacao de env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0', // Facilitar o uso com o frontend
-  port: 3333,
+  port: env.PORT,
 }).then(() => {
   console.log('🔥 Server is running on port 3333');
 });
