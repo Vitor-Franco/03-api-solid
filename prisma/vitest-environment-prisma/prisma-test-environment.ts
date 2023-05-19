a28205675dd6fc54c8f4ede478dcfe766324f98b
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-05-19 09:43:09 -0300

    test(e2e): adiciona ambiente para rodar os testes isolados

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,48 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    // Utilizamos o deploy, para o prisma não validar mudanças de schema e gerar migrations
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {
-        console.log('teardown')
+      // ao finalizar o teste, deletamos o schema e o banco de dados
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
 }
+
+// Objetivo desse arquivo:
+// - Criar um banco de dados para cada teste
+// - Criar um schema para cada teste
+// - Criar um arquivo de configuração para cada teste
+// - Isolar os testes
